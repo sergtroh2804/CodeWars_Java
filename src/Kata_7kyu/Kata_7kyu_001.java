fbfcdd73be7a9fbfceac6ffcdef5940ca85cbe3a
sergtroh2804 <sergtroh@yandex.ru>
2023-08-29 11:02:52 +0300

    IntelliJ IDEA add kata7kyu_001

---
+++ src/Kata_7kyu/Kata_7kyu_001.java
@@ -0,0 +1,19 @@
+package Kata_7kyu;
+/*
+Is this a triangle?
+
+Implement a function that accepts 3 integer values a, b, c.
+The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+
+ */
+public class Kata_7kyu_001 {
+    public static boolean isTriangle(int a, int b, int c){
+        boolean result = true;
+        if (a >= b + c) result = false;
+        if (b >= a + c) result = false;
+        if (c >= b + c) result = false;
+        return result;
+    }
+}
---
+++ src/Kata_7kyu/Kata_7kyu_001_test.java
@@ -0,0 +1,11 @@
+package Kata_7kyu;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class Kata_7kyu_001_test {
+    @Test
+    public void publicTests() {
+        assertEquals(true, Kata_7kyu_001.isTriangle(1,2,2));
+        assertEquals(false, Kata_7kyu_001.isTriangle(7,2,2));
+    }
+}
