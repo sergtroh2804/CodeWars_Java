a5a5d1b2f578e72e5ead19aacef5e2c9e36e5d97
sergtroh2804 <sergtroh@yandex.ru>
2023-09-12 10:54:51 +0300

    IntelliJ IDEA Add Kata_8kyu_011

---
+++ src/Kata_8kyu/Kata_8kyu_011.java
@@ -0,0 +1,8 @@
+package Kata_8kyu;
+
+public class Kata_8kyu_011 {
+    // Write a public static function `greet` that returns "hello world!"
+    public static String greet(){
+        return "hello world!";
+    }
+}
---
+++ src/Kata_8kyu/Kata_8kyu_011_test.java
@@ -0,0 +1,12 @@
+package Kata_8kyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Kata_8kyu_011_test {
+    @Test
+    public void testHelloWorld() throws Exception {
+        assertEquals("hello world!", Kata_8kyu_011.greet());
+    }
+}
