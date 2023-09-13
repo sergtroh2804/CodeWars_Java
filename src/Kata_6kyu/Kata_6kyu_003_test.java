ed8db1d6ae92eb007f808525f517399943078866
sergtroh2804 <sergtroh@yandex.ru>
2023-09-13 15:35:07 +0300

    IntelliJ IDEA Add Kata_6kyu_003

---
+++ src/Kata_6kyu/Kata_6kyu_003_test.java
@@ -0,0 +1,25 @@
+package Kata_6kyu;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class Kata_6kyu_003_test {
+    private static void testing(long a, long b, long[] res) {
+        assertEquals(Arrays.toString(res),
+                Arrays.toString(Kata_6kyu_003.sumDigPow(a, b).toArray()));
+    }
+    @Test
+    public void test() {
+        System.out.println("Basic Tests");
+        testing(1, 10, new long[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+        testing(1, 100, new long[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 89});
+        testing(10, 100, new long[] {89});
+        testing(90, 100, new long[] {});
+        testing(90, 150, new long[] {135});
+        testing(50, 150, new long[] {89, 135});
+        testing(10, 150, new long[] {89, 135});
+    }
+}
